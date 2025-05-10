ad37d6ae08667f541a75465904b10440b7c1be73
Jackson Njihia <njihiajack7son@gmail.com>
2025-05-10 03:35:28 +0300

    populated setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-auth-plugin',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Django>=3.0,<4.0',
+        'django-allauth',
+    ],
+    description='A plugin for user authentication, signup, login, email confirmation, and password reset.',
+    author='Jackson Njihia',
+    author_email='njihiajack7son@gmail.com',
+    url='https://github.com/your-repo-url',
+)
